e0c9ce5b2b9425580a16ba5d52759935f21e5c11
fgramon <ramon@vixloglogistica.com.br>
2026-09-21 13:43:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker simples: guarda só a "casca" (tela de abertura e ícones).
 // O app de conferência em si sempre vem do Google, sempre na versão mais nova.
-const CACHE = 'casca-conferencia-v1';
+const CACHE = 'casca-conferencia-v2';
 const ARQS = ['./', './index.html', './manifest.json', './icones/logo.png', './icones/icone-192.png', './icones/icone-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
